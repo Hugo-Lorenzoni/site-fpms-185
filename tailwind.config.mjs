165c5e8574d735b1db4d1e3a5eedf16eb73e5be6
Hugo Lorenzoni <210937@umons.ac.be>
2023-12-09 23:05:15 +0100

    update

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -5,6 +5,13 @@ const plugin = require("tailwindcss/plugin");
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
+    container: {
+      center: true,
+      padding: "1rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
     extend: {
       fontFamily: {
         sans: ["PoppinsVariable", "Poppins", ...defaultTheme.fontFamily.sans],
