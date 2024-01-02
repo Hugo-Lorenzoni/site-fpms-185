cb24cbd410c9a193b943cf2c3f8dd21ac6237792
Hugo Lorenzoni <210937@umons.ac.be>
2024-01-02 01:24:28 +0100

    speed insights

--- astro.config.mjs
+++ astro.config.mjs
@@ -13,18 +13,8 @@ export default defineConfig({
     webAnalytics: {
       enabled: true,
     },
+    speedInsights: {
+      enabled: true,
+    },
   }),
-  // vite: {
-  //   define: {
-  //     "import.meta.env.PUBLIC_VERCEL_ANALYTICS_ID": JSON.stringify(
-  //       process.env.VERCEL_ANALYTICS_ID,
-  //     ),
-  //   },
-  // },
-  // serverOptions: {
-  //   headers: {
-  //     "Content-Security-Policy":
-  //       "default-src 'self'; script-src 'self' https://vitals.vercel-insights.com; style-src 'self'; img-src 'self' https://youtube.com;",
-  //   },
-  // },
 });
