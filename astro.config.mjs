af28eb4a481fcfcb48edafa3257d290a991e603a
Hugo Lorenzoni <210937@umons.ac.be>
2024-01-02 01:04:59 +0100

    add analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,26 @@
 import { defineConfig } from "astro/config";
-import react from "@astrojs/react";
 
+import vercel from "@astrojs/vercel/static";
+
+import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind()],
+  output: "static",
+  adapter: vercel({ analytics: true }),
+  // vite: {
+  //   define: {
+  //     "import.meta.env.PUBLIC_VERCEL_ANALYTICS_ID": JSON.stringify(
+  //       process.env.VERCEL_ANALYTICS_ID,
+  //     ),
+  //   },
+  // },
+  // serverOptions: {
+  //   headers: {
+  //     "Content-Security-Policy":
+  //       "default-src 'self'; script-src 'self' https://vitals.vercel-insights.com; style-src 'self'; img-src 'self' https://youtube.com;",
+  //   },
+  // },
 });
