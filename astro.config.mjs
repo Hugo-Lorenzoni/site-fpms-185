3e153a65caf4739706c7e93e562dc0a9bbda5feb
Hugo Lorenzoni <210937@umons.ac.be>
2024-01-21 15:49:43 +0100

    robotsTxt

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,7 @@ import { defineConfig } from "astro/config";
 import vercel from "@astrojs/vercel/static";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
-
-import sitemap from "@astrojs/sitemap";
+import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
@@ -14,7 +13,7 @@ export default defineConfig({
       // Useful if you need to define and/or import your own custom `base.css`.
       applyBaseStyles: false,
     }),
-    sitemap(),
+    robotsTxt(),
   ],
   site: "https://fede.fpms.ac.be",
   output: "static",
